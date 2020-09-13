f604c8e169c022f116bb7ba5a132a2f798769581
unknown <ncuti60@gmail.com>
2020-09-13 20:17:03 +0200

    ft:update controllers

--- controllers/houseControllers.js
+++ controllers/houseControllers.js
@@ -1,24 +1,16 @@
 const models = require('../database/models');
 
-exports.getAllHouses = async (req, res, next) => {
-    try {
-        const Houses = await models.House.findAll({
-            include: [
-                {
-                    model: models.User,
-                    as: 'landlord'
-                }
-            ]
-        })
-        res.status(200).send({
-            status: 'success',
-            Houses
-        })
-    } catch (error) {
-        res.status(400).send({
-            status: 'Fail',
-            message: error.message
-        })
-    }
-    next()
+exports.getAllHouses = async (req, res) => {
+    const Houses = await models.House.findAll({
+        include: [
+            {
+                model: models.User,
+                as: 'landlord'
+            }
+        ]
+    })
+    res.status(200).send({
+        status: 'success',
+        Houses
+    })
 }
