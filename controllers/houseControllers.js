583e4af0079490a0ebee55d7b0c4a04d360a0f8a
unknown <ncuti60@gmail.com>
2020-09-13 19:53:37 +0200

    ft: update controller

--- controllers/houseControllers.js
+++ controllers/houseControllers.js
@@ -12,10 +12,10 @@ exports.getAllHouses = async (req, res, next) => {
         })
         res.status(200).send({
             status: 'success',
-            Houses
+            Houses: Houses
         })
     } catch (error) {
-        res.send({
+        res.status(404).send({
             status: 'Fail',
             message: error.message
         })
